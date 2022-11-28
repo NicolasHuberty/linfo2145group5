68a370d5a60fd12845743541bd6667153873fc7d
NicolasHuberty <46087013+NicolasHuberty@users.noreply.github.com>
2022-11-28 10:58:43 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 wget https://raw.githubusercontent.com/NicolasHuberty/linfo2145group5/main/scapp.yml
-temp = docker swarm init --advertise-addr 192.168.56.109 | grep -n "docker swarm join --token" | cut -b 7-
+temp=$(docker swarm init --advertise-addr 192.168.56.109 | grep -n "docker swarm join --token" | cut -b 7-)
 echo "You have to copy this in worker and adding  --advertise-addr `curl ifconfig.me`"
 echo ${temp}
