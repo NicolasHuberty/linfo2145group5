3530ab326538f818090414d484005881c65d04d3
NicolasHuberty <46087013+NicolasHuberty@users.noreply.github.com>
2022-11-28 22:12:45 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ temp=$(docker swarm init --advertise-addr `curl ifconfig.me` | grep -n "docker s
 echo "You have to copy this in worker and then go back here"
 echo ${temp} "--advertise-addr \`curl ifconfig.me\`"
 echo "PRESS OK when the command above is done"
-read ok
+read -r -s -p $'Press escape to continue...\n' -d $'\e'
 docker network create --driver overlay --attachable scapp-net
 docker stack deploy -c scapp.yml scapp
 docker run -d -e REACT_APP_AUTH_SERVICE_URL=http://`curl ifconfig.me`:3001 -e REACT_APP_CATALOG_SERVICE_URL=http://`curl ifconfig.me`:3004 -e REACT_APP_BASKET_SERVICE_URL=http://`curl ifconfig.me`:3008 -p 3002:80 --name scapp-front nicolashuberty/scapp-frontend
