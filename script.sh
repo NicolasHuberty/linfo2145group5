022bdaa555bd3c5adff1d203ddd63cbf7727fb86
NicolasHuberty <46087013+NicolasHuberty@users.noreply.github.com>
2022-11-28 11:08:20 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 wget https://raw.githubusercontent.com/NicolasHuberty/linfo2145group5/main/scapp.yml
 temp=$(docker swarm init --advertise-addr 192.168.56.109 | grep -n "docker swarm join --token" | cut -b 7-)
-echo "You have to copy this in worker"
+echo "You have to copy this in worker and then go back here"
 echo ${temp} "--advertise-addr \`curl ifconfig.me\`"
+docker network create --driver overlay --attachable scapp-net
+docker stack deploy -c scapp.yml scapp
