b40d5983a60bdf310d9b9008bb784981a4d1125b
NicolasHuberty <46087013+NicolasHuberty@users.noreply.github.com>
2022-11-28 12:26:12 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,15 @@
 wget https://raw.githubusercontent.com/NicolasHuberty/linfo2145group5/main/scapp.yml
-temp=$(docker swarm init --advertise-addr 192.168.56.109 | grep -n "docker swarm join --token" | cut -b 7-)
+temp=$(docker swarm init --advertise-addr `curl ifconfig.me` | grep -n "docker swarm join --token" | cut -b 7-)
 echo "You have to copy this in worker and then go back here"
 echo ${temp} "--advertise-addr \`curl ifconfig.me\`"
 echo "PRESS OK when the command above is done"
 read ok
 docker network create --driver overlay --attachable scapp-net
 docker stack deploy -c scapp.yml scapp
+docker run -d \
+-e REACT_APP_AUTH_SERVICE_URL=http://`curl ifconfig.me`:3001 \
+-e REACT_APP_CATALOG_SERVICE_URL=http://`curl ifconfig.me`:3004 \
+-e REACT_APP_BASKET_SERVICE_URL=http://`curl ifconfig.me`:3008 \
+-p 3002:80 --name scapp-front scapp-front
+
+
